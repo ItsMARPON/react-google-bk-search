507628512650b9a95671968fb6e4e11d7f2c0f04
Mary <itsmaryyang@gmail.com>
2023-05-11 14:13:41 -0500

    Add user to query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,69 +1,70 @@
-const User = require("../models/User");
+const { User, bookSchema } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
-
 const resolvers = {
-    Query: {
-      me: async (parent, args, context) => {
-        if (context.user) {
-          const foundUser = await User.findOne({ _id: context.user._id }).select(
-            "-__v -password"
-          );
-          return foundUser;
-        }
-        throw new AuthenticationError("You are not logged in!");
-      },
+  Query: {
+    user: async (parents, {username})=> {
+      return User.findOne({username}).populate('savedBooks');
     },
-    Mutation: {
-      login: async (parent, {email, password}) => {
-        const user = await User.findOne({ email: email });
-  
-        if (!user) {
-          throw new AuthenticationError("Can't find this user");
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError("Wrong Password!");
-        }
-        const token = signToken(user);
-        return { token, user };
-      },
-      addUser: async (parent, {username, email, password}) => {
-        const user = await User.create({username, email, password});
-        const token = signToken(user);
-        return {token, user}
-  
-      },
-      saveBook: async (parent, args, context) => {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: user._id },
-            { $addToSet: { savedBooks: args.bookInput } },
-            { new: true, runValidators: true }
-          );
-          return updatedUser;
-        }
-  
-        throw new AuthenticationError("You are not logged in!");
-      },
-      removeBook: async (parent, args, context) => {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: user._id },
-            { $pull: { savedBooks: { bookId: args.bookId } } },
-            { new: true }
-          );
-          if (!updatedUser) {
-            throw new Error("Couldn't find user with this id!");
-          }
-          return updatedUser;
-        }
-        throw new AuthenticationError("You are not logged in!");
-      },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const foundUser = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return foundUser;
+      }
+      throw new AuthenticationError("You are not logged in!");
     },
-  };
-  
-  module.exports = resolvers;
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong Password!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: args.bookInput } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You are not logged in!");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+          throw new Error("Couldn't find user with this id!");
+        }
+        return updatedUser;
+      }
+      throw new AuthenticationError("You are not logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
