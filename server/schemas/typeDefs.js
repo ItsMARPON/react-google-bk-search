b53a053e350e513f0919d0c2577a25be94088f3a
Mary <itsmaryyang@gmail.com>
2023-05-11 14:03:34 -0500

    Format document in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,42 +1,42 @@
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String!
     bookCount: Int
     savedBooks: [Book]
-}
-type Book{
+  }
+  type Book {
     bookId: ID!
     authors: [String]
     description: String!
     title: String!
     image: String
     link: String
-}
-type Auth{
+  }
+  type Auth {
     token: ID!
     user: User
-}
-input BookInput{
+  }
+  input BookInput {
     authors: [String]
     description: String!
     title: String!
     bookId: String!
     image: String
     link: String
-}
-type Query{
+  }
+  type Query {
     me: User
-}
-type Mutation{
-    login(email:String!, password:String!): Auth
-    addUser(username:String!, email:String!, password:String): Auth
-    saveBook(bookInput:BookInput!):User
-    removeBook(bookId:ID!):User
-}
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String): Auth
+    saveBook(bookInput: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
