49a9d723e27e2ac54b4846617981721776215c2d
Mary <itsmaryyang@gmail.com>
2023-05-15 16:59:48 -0500

    edit code for SavedBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -71,14 +71,14 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    // const token = Auth.loggedIn() ? Auth.getToken() : null;
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    // if (!token) {
-    //   return false;
-    // }
+    if (!token) {
+      return false;
+    }
 
     try {
-      const data = await saveBook({variables: {bookToSave},
+      const data = await saveBook({variables: {input: bookToSave},
       });
 
       // if book successfully saves to user's account, save book id to state
